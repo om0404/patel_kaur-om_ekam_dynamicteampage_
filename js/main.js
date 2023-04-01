c0c1610a943dc4fa81d2d98e8908ccc86d9e6625
om0404 <o_patel164575@fanshaweonline.ca>
2023-04-01 00:21:31 -0400

    finishing_om_artwork
    completed

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let OM ={
+    om: {
+        name:'OM patel',
+        headline:'DESIGNER',
+        bio:'HELLO,  my name is om,i am intrested in design since very young age,i love to design things and i like it a lot.'  
+    },
+}
